26f352134bfafee72bcf35b27446718f5f8c2c32
Alefrank <alefrank.m@gmail.com>
2022-07-14 13:59:41 -0400

    change url

--- frontend/src/lib/components/CoverPhoto.svelte
+++ frontend/src/lib/components/CoverPhoto.svelte
@@ -1,36 +1,8 @@
 <script>
-	import { session } from '$app/stores'
-	import { HOST_DEFAULT } from '$lib/hooks/useFetch'
-
-	// console.log($session);
-	let src = null
-	let srcUploaded = null
-	export let coverPhoto = null
-	async function uploadSrc() {
-		const [file] = src.files
-
-		srcUploaded.src = URL.createObjectURL(file)
-		// alert("Multimedia subida correctamente");
-
-		if (file) {
-			const formData = new FormData()
-			formData.append('profile-photo', file)
-			formData.append('id', $session.id)
-
-			await fetch(`${HOST_DEFAULT}/api/user/change-cover-photo`, {
-				body: formData,
-				method: 'POST',
-			})
-		}
-	}
 </script>
 
 <div>
-	<img
-		src={coverPhoto ? `${HOST_DEFAULT}/files/${coverPhoto}` : '/img/voyer/encabezado.jpg'}
-		bind:this={srcUploaded}
-		alt="img"
-	/>
+	<img src="'/img/voyer/encabezado.jpg'" alt="img" />
 </div>
 
 <style>
--- frontend/src/lib/components/UserPhoto.svelte
+++ frontend/src/lib/components/UserPhoto.svelte
@@ -1,58 +1,20 @@
 <script>
 	import { session } from '$app/stores'
-
-	// import { session } from '$app/stores'
-	import { HOST_DEFAULT } from '$lib/hooks/useFetch'
-	import LinkToUser from './Links/LinkToUser.svelte'
-	let src = null
-	let srcUploaded = null
-	export let user = null
-
-	async function uploadSrc() {
-		const [file] = src.files
-
-		srcUploaded.src = URL.createObjectURL(file)
-		// alert("Multimedia subida correctamente");
-
-		if (file) {
-			const formData = new FormData()
-			formData.append('profile-photo', file)
-			formData.append('id', user.id)
-
-			await fetch(`${HOST_DEFAULT}/api/user/change-profile-photo`, {
-				body: formData,
-				method: 'POST',
-			})
-		}
-	}
 </script>
 
 <main>
 	<div class="details">
 		<span>
-			{user.firstName}
-			{user.lastName}
+			{$session.fullName}
 		</span>
 
-		<span>@{user.username}</span>
+		<span>@{$session.username}</span>
 	</div>
 
 	<div class="avatar">
 		<figure>
-			<img
-				src={user.profilePhoto
-					? `${HOST_DEFAULT}/files/${user.profilePhoto}`
-					: '/img/voyer/profile.png'}
-				bind:this={srcUploaded}
-				alt=""
-			/>
+			<img src="/img/voyer/profile.png" alt="" />
 		</figure>
-		{#if user.id === $session._id}
-			<label class="hover-layer">
-				<input required name="file" bind:this={src} type="file" hidden on:change={uploadSrc} />
-				<i class="fas fa-edit" />
-			</label>
-		{/if}
 	</div>
 </main>
 
@@ -100,26 +62,5 @@
 		object-position: top;
 		width: 100%;
 		height: 100%;
-	}
-
-	.hover-layer {
-		position: absolute;
-		top: 0;
-		right: 0;
-		width: 100%;
-		height: 100%;
-
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		color: transparent;
-		font-size: 16px;
-	}
-
-	.hover-layer:hover {
-		background-color: rgb(0, 0, 0, 0.5);
-		cursor: pointer;
-		color: #fff;
-		transition: 0.2s ease all;
 	}
 </style>
