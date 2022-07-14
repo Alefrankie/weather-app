5a9aab2634223421154d703630aca5cdef961986
Alefrankie <alefrank.m@gmail.com>
2022-07-14 07:25:05 -0400

    add favorite to main module

--- backend/src/App/app.module.ts
+++ backend/src/App/app.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { UserModule } from 'src/User/user.module'
 import { AppController } from './app.controller'
+import { FavoritesModule } from 'src/Favorites/favorites.module'
 
 @Module({
 	imports: [
 		HistoryModule,
 		MongooseModule.forRoot('mongodb://localhost/voyer-club'),
-		UserModule
+		UserModule,
+		FavoritesModule
 	],
 	controllers: [AppController],
 	providers: []
